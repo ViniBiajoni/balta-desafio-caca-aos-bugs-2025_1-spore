a54f841fabca0eb8b4bac976b9593ee89beeb37c
ViniBiajoni <vinibiajoni@gmail.com>
2025-10-29 20:21:45 -0300

    Migrations

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,17 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if(string.IsNullOrEmpty(connectionString))
+    throw new Exception("Connection string not found");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+db.Database.Migrate();
 
 app.MapGet("/", () => "Hello World!");
 
